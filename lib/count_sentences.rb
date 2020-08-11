7c972c7312f87decc0d700591aa6df4bacc35a9d
Wayne del Corral Jr <wdelcorjr@gmail.com>
2020-08-10 22:45:15 -0500

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,23 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    split = self.split(/[.?!]/)
+    if split.count > 0
+      split.reject { |word| word == ""}.count
+    else
+      0
+    end
   end
 end
